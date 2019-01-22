68f53aeaba80faf38316ebd69a4ff131f81e43ec
Hunter Burden <hb193317@ohio.edu>
2019-01-22 11:51:04 -0500

    intial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hunter Burden
+ *        Date: January 22, 2019
+ * Description: Tells us hello git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
